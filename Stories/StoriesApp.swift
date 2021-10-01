6fc17ed2a501efae5dd03a528e88a18f8dc2e8f6
yuan <dnstylish@gmail.com>
2021-10-02 01:05:00 +0700

    Initial Commit

---
+++ Stories/StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesApp.swift
+//  Stories
+//
+//  Created by Yuan on 02/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
